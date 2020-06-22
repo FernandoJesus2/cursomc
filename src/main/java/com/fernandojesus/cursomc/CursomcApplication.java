6e5dab0c7f3343d15fc5d833c2fc28ad67f3210c
Fernando Jesus <fernando.fj02@hotmail.com>
2020-06-21 21:06:54 -0300

    Projeto criado

---
+++ src/main/java/com/fernandojesus/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.fernandojesus.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//teste
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
